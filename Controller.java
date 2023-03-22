6a1313eebf18ebe9a64540b94d668bb9ebcacea0
Vincent <vincent.poiatti@etu.univ-poitiers.fr>
2023-03-22 09:53:53 +0100

    Controller

---
+++ Controller.java
@@ -0,0 +1,38 @@
+
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * Write a description of JavaFX class StepTwoController here.
+ *
+ * @author Patrick GIRARD
+ * @version 22/03/2020
+ */
+public class Controller
+{
+    // We keep track of the count, and label displaying the count:
+    
+    @FXML
+    private Label myLabel;
+    
+    /**
+     * This will be executed when the button is clicked
+     * It increments the count by 1
+     */
+    @FXML
+    private void buttonClickList(ActionEvent event)
+    {
+        
+    }
+}
